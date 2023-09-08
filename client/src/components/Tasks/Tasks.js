d997815a20fb7bdbaebd815df38066cf1700ca8d
CMcManamon <66437184+CMcManamon@users.noreply.github.com>
2023-09-08 08:31:28 -0700

    Tasks are sorted by due date

--- client/src/components/Tasks/Tasks.js
+++ client/src/components/Tasks/Tasks.js
@@ -1,11 +1,18 @@
 import Task from "./Task/Task";
 import { useSelector } from "react-redux";
 import { Stack, CircularProgress } from "@mui/material";
+import moment from "moment";
 
 const Tasks = ({ setCurrentId }) => {
   const tasks = useSelector((state) => state.tasks);
   let rows = [];
   if (tasks === undefined) return "";
+
+  /* Sort tasks by due date
+    TODO: sorting function that takes into account priority and period
+      e.g. a weekly task overdue is more important than a yearly task overdue by the same amount
+  */
+  tasks.sort((a, b) => moment(a.dueDate).diff(moment(b.dueDate)));
   tasks.forEach((task) => {
     rows.push(
       <Task key={task._id} task={task} setCurrentId={setCurrentId}></Task>
