5c8a39ab48292b0b4c9d29126b9409fa1ee06552
CMcManamon <66437184+CMcManamon@users.noreply.github.com>
2023-10-01 18:57:16 -0700

    Implement task sorting function

--- client/src/components/Tasks/Tasks.js
+++ client/src/components/Tasks/Tasks.js
@@ -1,7 +1,7 @@
 import Task from "./Task/Task";
 import { useSelector } from "react-redux";
 import { Stack, CircularProgress } from "@mui/material";
-import moment from "moment";
+import { sortTasks } from "../../helpers/sortTasks";
 
 const Tasks = () => {
   const tasks = useSelector((state) => state.tasks);
@@ -12,7 +12,7 @@ const Tasks = () => {
     TODO: sorting function that takes into account priority and period
       e.g. a weekly task overdue is more important than a yearly task overdue by the same amount
   */
-  tasks.sort((a, b) => moment(a.dueDate).diff(moment(b.dueDate)));
+  tasks.sort(sortTasks());
   tasks.forEach((task) => {
     rows.push(<Task key={task._id} task={task}></Task>);
   });
---
+++ client/src/helpers/sortTasks.js
@@ -0,0 +1,46 @@
+const MILLISECONDS_IN_DAY = 8.64e7;
+const PRIORITY_MULTIPLIER = [0.25, 0.75, 1.0, 1.25, 1.75];
+export const sortTasks = () => {
+  return function (a, b) {
+    const priorityA = taskPriority(a);
+    const priorityB = taskPriority(b);
+
+    // Same calculated priority based on due date, defer to task priority setting
+    if (priorityA == priorityB) return b.priority - a.priority;
+    // Neither is due, sort by highest priority
+    else if (priorityA > 0 && priorityB > 0) return priorityB - priorityA;
+    // At least 1 task is overdue; sort by most negative
+    else return priorityA - priorityB;
+  };
+};
+
+const taskPriority = (task) => {
+  if (!task) return 0;
+  // Get diff between now and the duedate, and divide by milliseconds in a day
+  let daysUntilDue = Math.round(
+    (new Date(task.dueDate).getTime() - Date.now()) / MILLISECONDS_IN_DAY
+  );
+
+  const frequency = task.recurring ? taskFrequency(task) : 1;
+  const priority =
+    (PRIORITY_MULTIPLIER[task.priority] * daysUntilDue) / frequency;
+
+  return priority;
+};
+
+const taskFrequency = (task) => {
+  const type = task.periodType;
+  const period = task.period;
+  switch (type) {
+    case "repeat_days":
+      return period * 1;
+    case "repeat_weeks":
+      return period * 7;
+    case "repeat_months":
+      return period * 30; // only need approximations
+    case "repeat_years":
+      return period * 365;
+    default:
+      return 1;
+  }
+};
