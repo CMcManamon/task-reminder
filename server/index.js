ae69daa802dc41a80f7c5f3beff5d81da45379da
CMcManamon <66437184+CMcManamon@users.noreply.github.com>
2023-11-05 19:59:02 -0800

    Add greeting route to API

--- server/index.js
+++ server/index.js
@@ -13,6 +13,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use("/tasks", taskRoutes);
 
+// Greeting route
+app.get("/", (req, res) => {
+  res.send("Task Manager API");
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
